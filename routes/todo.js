63722a8ce337f088371628130e5878acc52ee12c
jenn14108 <jenn.lee14108@gmail.com>
2018-06-12 14:17:28 -0400

    Add get and post function for todo page

--- routes/todo.js
+++ routes/todo.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('todo', { title: 'To-Do' });
+  console.log("Inside get")
+  res.render('todo', { title: 'To-Do', todo_task:0});
+});
+
+router.post('/', function(req, res, next) {
+  console.log("Inside post")
+  const todo_task = req.body.todo_task
+  //database.squares.push(task)
+  console.log(req.body.todo_task)
+//  fs.writeFileSync('../database/data.json',JSON.stringify(database,null,' '));
+  res.render('todo', { title: 'To-Do', todo_task:todo_task});
 });
 
 module.exports = router;
